c672feb3029895e5ee89aafba0e2db60ef0dba75
alireza kasirzare <alirezakasir@yahoo.com>
2022-11-15 09:57:47 +0330

    edit ticket pages

--- pages/panel/touristLeader/tiket/_tiketId/index.vue
+++ pages/panel/touristLeader/tiket/_tiketId/index.vue
@@ -7,15 +7,12 @@
             class="col-12 d-flex justify-content-end px-0"
             v-if="message.sender == 'tourleader'"
           >
-            <div
-              class="bg-info text-light rounded border px-2"
-              style="max-width: 50%"
-            >
+            <div class="bg-info text-light rounded border px-2 card-message me">
               {{ message.text }}
             </div>
           </div>
           <div class="col-12 px-0 d-flex" v-if="message.sender == 'tourist'">
-            <div class="bg-light rounded border px-2" style="max-width: 50%">
+            <div class="bg-light rounded border px-2 card-message other">
               {{ message.text }}
             </div>
           </div>
@@ -58,21 +55,25 @@
         class="btn btn-info pointer mx-2"
         @click="nextprev(messages.previous)"
       >
-        prev
+        <i class="fa fa-chevron-left"></i>
       </button>
       <button
         class="btn btn-info pointer mx-2"
         @click="nextprev(messages.next)"
       >
-        next
+        <i class="fa fa-chevron-right"></i>
       </button>
     </div>
     <div
       class="row mx-0 col-12 justify-content-center my-3"
       v-if="messages.previous == null && messages.next == null"
     >
-      <button class="bg-light mx-2 border rounded">prev</button>
-      <button class="bg-light mx-2 border rounded">next</button>
+      <button class="bg-light mx-2 border rounded">
+        <i class="fa fa-chevron-left"></i>
+      </button>
+      <button class="bg-light mx-2 border rounded">
+        <i class="fa fa-chevron-right"></i>
+      </button>
     </div>
     <div
       class="row mx-0 col-12 justify-content-center my-3"
@@ -82,30 +83,26 @@
         class="btn btn-info pointer mx-2"
         @click="nextprev(messages.previous)"
       >
-        prev
+        <i class="fa fa-chevron-left"></i>
       </button>
-      <button class="bg-light mx-2 border rounded">next</button>
+      <button class="bg-light mx-2 border rounded">
+        <i class="fa fa-chevron-right"></i>
+      </button>
     </div>
     <div
       class="row mx-0 col-12 justify-content-center my-3"
       v-if="messages.previous == null && messages.next != null"
     >
-      <button class="bg-light mx-2 border rounded">prev</button>
+      <button class="bg-light mx-2 border rounded">
+        <i class="fa fa-chevron-left"></i>
+      </button>
       <button
         class="btn btn-info pointer mx-2"
         @click="nextprev(messages.next)"
       >
-        next
+        <i class="fa fa-chevron-right"></i>
       </button>
     </div>
-    <!-- <textarea
-      class="col-12"
-      cols="30"
-      rows="2"
-      v-model="text"
-      placeholder="text"
-    ></textarea>
-    <button class="btn btn-success mt-3" @click="sendMessage">send</button> -->
     <input
       type="file"
       class="col-12 p-0 d-none"
@@ -165,5 +162,34 @@ export default {
   },
 };
 </script>
+<style>
+.card-message {
+  width: 40%;
+  min-height: 100px;
+  position: relative;
+}
 
-<style></style>
+.card-message.me::after {
+  position: absolute;
+  right: -10px;
+  top: 20px;
+  content: "";
+  width: 0;
+  height: 0;
+  border-top: 5px solid transparent;
+  border-left: 10px solid #17a2b8 !important;
+  border-bottom: 5px solid transparent;
+}
+
+.card-message.other::after {
+  position: absolute;
+  left: -10px;
+  top: 20px;
+  content: "";
+  width: 0;
+  height: 0;
+  border-top: 5px solid transparent;
+  border-right: 10px solid #eee !important;
+  border-bottom: 5px solid transparent;
+}
+</style>
